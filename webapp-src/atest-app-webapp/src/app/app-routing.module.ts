33e1c0a1f90e1ce4fffc0415ccbb0d40aa3b9b9a
MaresR0bert <robi_mares@yahoo.com>
2023-04-17 17:41:42 +0300

    1. What does this change do? added routing to app 2. Side effects? N/A

--- webapp-src/atest-app-webapp/src/app/app-routing.module.ts
+++ webapp-src/atest-app-webapp/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
+import {SignupFormComponent} from "./signup-form/signup-form.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginFormComponent}
+  {path: '', redirectTo: 'login', pathMatch:'full'},
+  {path: 'login', component: LoginFormComponent, pathMatch: 'full'},
+  {path: 'signup', component: SignupFormComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- webapp-src/atest-app-webapp/src/app/app.module.ts
+++ webapp-src/atest-app-webapp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    SignupFormComponent,
   ],
   imports: [
     BrowserModule,
