d863e474d0a7d0e23846eab395bd26e6e3c8850e
MaresR0bert <robi_mares@yahoo.com>
2023-06-19 22:52:44 +0300

    Added Question add to DB

--- src/services/accessTokenVerifyService.ts
+++ src/services/accessTokenVerifyService.ts
@@ -3,7 +3,8 @@ import responseFactory from "../util/responseFactory";
 
 const verifyAccessToken = async (req: any, res: any, next: any) => {
     try {
-        await <JwtPayload>jwt.verify(req.get('Authorization').split(" ")[1], process.env.ACCESS_TOKEN_SECRET!);
+        const accessToken: JwtPayload = await <JwtPayload>jwt.verify(req.get('Authorization').split(" ")[1], process.env.ACCESS_TOKEN_SECRET!);
+        res.locals.userId = accessToken.userId;
         next();
     } catch (err) {
         return responseFactory(res, 401, {error: err});
--- src/services/questionService.ts
+++ src/services/questionService.ts
@@ -1,11 +1,21 @@
 import logger from "../util/logger";
 import responseFactory from "../util/responseFactory";
 import * as CryptoJS from "crypto-js";
+import Question from "../schemas/questionSchema";
+
+const decryptQuestion = (encryptedQuestion: string): any => {
+    const decryptedData =
+        CryptoJS.AES.decrypt(decodeURIComponent(encryptedQuestion), process.env.AES_ENCRYPTION_KEY!);
+    return JSON.parse(decryptedData.toString(CryptoJS.enc.Utf8));
+}
 
 const addQuestion = async (req: any, res: any) => {
-    const decryptedData = CryptoJS.AES.decrypt(decodeURIComponent(req.body.payload), process.env.AES_ENCRYPTION_KEY!);
-    const decryptedInfo = JSON.parse(decryptedData.toString(CryptoJS.enc.Utf8));
-    logger.info(JSON.stringify(decryptedInfo));
+    let question = decryptQuestion(req.body.payload);
+    Object.assign(question, {owner: res.locals.userId});
+
+    const questionDoc = new Question(question);
+    questionDoc.save();
+
     return responseFactory(res, 200, {});
 }
 
